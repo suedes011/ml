301ce3e14258c56a3df2aef7c7e42afb50d11eec
suedes011 <102919603+suedes011@users.noreply.github.com>
2022-09-06 08:10:33 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ print("MENU")
 print("------------------------------------------")
 print("1. SpamHam Sigmoid")
 print("2. Naive Bayes")
+print("3. Perceptron")
 
 ch = int(input("Enter your choice : "))
 file = ""
@@ -11,6 +12,8 @@ if ch == 1:
     file = "spam"
 elif ch == 2:
     file = "nb"
+elif ch == 3:
+    file = "pt"
 else:
     print("wrong option")
     
